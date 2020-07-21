89dd64bfa5a97477dbdc5b0c7a00d7bf4693da33
fermin <fyflzjz@aliyun.com>
2020-07-22 00:13:31 +0800

    init

---
+++ xhprof_lib/defaults.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('XHPROF_LIB_ROOT')) {
+  define('XHPROF_LIB_ROOT', dirname(__FILE__));
+}
+
+if (!defined('XHPROF_CONFIG')) {
+  define('XHPROF_CONFIG', XHPROF_LIB_ROOT . '/config.php');
+}
