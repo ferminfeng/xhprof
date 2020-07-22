6e1d32d7cac7bdb5f2578f72e9bd3292a4baee06
fermin <fyflzjz@aliyun.com>
2020-07-22 11:36:18 +0800

    init

--- xhprof_lib/utils/xhprof_runs.php
+++ xhprof_lib/utils/xhprof_runs.php
@@ -88,7 +88,6 @@ class XHProfRuns_Default implements iXHProfRuns {
   {
 	global $_xhprof;
 	require_once XHPROF_LIB_ROOT.'/utils/Db/'.$_xhprof['dbadapter'].'.php';
-	
     $class = self::getDbClass();
     $this->db = new $class($_xhprof);
     $this->db->connect();
